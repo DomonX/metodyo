17a52a6abcc994a2e08e533f5a6facca92fbff69
DomonX <norbiix1@gmail.com>
2020-11-09 10:51:12 +0100

    feat: horner

--- src/app/lab1/lab1.component.ts
+++ src/app/lab1/lab1.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Lab1Component implements OnInit {
 
+  result: number;
+  textRes: string[];
+
+  values: string;
+  x: number;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  horner(): void {
+    this.textRes = [];
+    const tab = this.values.split(' ').map(x => Number.parseFloat(x));
+    this.result = tab.reduce((acc, curr, index) => {
+      const res = acc * this.x + curr;
+      this.textRes.push(`${acc} * ${this.x} + ${curr} = ${res}`);
+      return res;
+    });
   }
 
 }
